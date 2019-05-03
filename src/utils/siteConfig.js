e0a2ae208fa9bed055983b2a71f46ca1945cc64b
mskian <veerfun23@gmail.com>
2019-05-03 11:04:12 +0530

    Update postsperpage

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,7 +1,7 @@
 module.exports = {
     siteUrl: `https://hello.tamilimages.xyz`, // Site domain. Do not include a trailing slash!
 
-    postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+    postsPerPage: 9, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `Tamil Status`, // This allows an alternative site title for meta data for pages.
     siteDescriptionMeta: `Tamil SMS, Kavithai and Quotes.`, // This allows an alternative site description for meta data for pages.
